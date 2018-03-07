74abcdf56f13491ba806b1b0f549564a041dc0de
51115152017.2 <51115152017.2@51DE194280.senacrjedu.local>
2018-03-06 22:03:35 -0300

    Primeiro commit

---
+++ tipo_dados.php
@@ -0,0 +1,33 @@
+<?php
+
+/* 
+ * Autor: Bergson
+ *Data: 06-03-2018
+ * Finalidade : Explanação  de tipos de dados
+ */
+
+# Variável  aramazenando  um valor de texto
+$texto ="Seguanda aula de PHP";
+#variável  aramazenado umn valor inteiro
+$inteiro = 02;
+#variável armazenando um valor real
+$real= 45.15;
+#Variável armazenando um caractere
+$letra = 'L';
+#variável armazenando um valor boleano
+$boleano = TRUE;
+
+
+var_dump($texto);
+echo "<br>";
+var_dump($inteiro);
+echo "<br>";
+var_dump($real);
+echo"<br>";
+var_dump($letra);
+echo"<br>";
+var_dump($boleano);
+
+
+        
+
