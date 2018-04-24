f5465afeb903068257ccfeb42d75f8196ac0d4da
51115152017.2 <51115152017.2@51DE194280.senacrjedu.local>
2018-04-24 19:20:24 -0300

    folha3

---
+++ exercicio16.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Exercicio 16</title>
+    </head>
+    <body>
+       <form action="#" method="POST">
+        <label for="frase">Frase</label>
+        <input type="number" name="frase">
+       </form>
+        <?php
+        $frase = $_POST['frase'];
+        $vogais [10]= "AaEeIiOoUu";
+        $aux = 0;
+        
+        
+        while($aux){
+        for($i=0;$i<10; $i++){
+            if($aux == $vogais[i]){
+              
+              $i++;
+  
+            $aux++;
+            
+            }   
+        }
+      }  
+        return $aux;
+      ?> 
+    </body>
+</html>
