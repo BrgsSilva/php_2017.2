ade7d13bba8e7d9ae5bdbec52ce73b00c2681f37
51115152017.2 <51115152017.2@51DE194247.senacrjedu.local>
2018-05-08 21:55:48 -0300

    aula03

---
+++ src/aula3/EstruturaDeDecisao.java
@@ -0,0 +1,15 @@
+package aula3;
+
+public class EstruturaDeDecisao {
+	public static void main(String[] args) {
+		
+		int var1 = 23;
+		int var2 = 24;
+		
+		if(var1 >= var2){
+			System.out.println("Maior");
+		}else {
+			System.out.println("Menor");
+		}
+	}
+}
---
+++ src/aula3/EstruturaDeDecisaoEncadeada.java
@@ -0,0 +1,27 @@
+package aula3;
+
+public class EstruturaDeDecisaoEncadeada {
+	public static void main(String[] args) {
+	/**
+	 * Estrutura de decisão
+	 * if()
+	 * if()e else()
+	 * if(), else if(), else
+	 **/
+	
+	/*Segundo exemplo */
+	
+	String materia = "Java";
+	
+	
+	if(materia == "PHP") {
+		System.out.println("Disciplina do 4º modulo");
+	} else if (materia == "banco de Dados") {
+		System.out.println("Disciplian do 3º modulo");
+	} else if(materia == "Java") {
+		System.out.println("Disciplina do 5º modulo");
+	} else {
+		System.out.println("Disciplina  naõ é dos modulo de progarmação");
+	}
+	}
+	}
