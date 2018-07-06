0d1e1b84b68e04dfc6559ff238125f520435380b
51115152017.2 <51115152017.2@51DE194280.senacrjedu.local>
2018-07-05 22:00:33 -0300

    aula 19

---
+++ src/banco/ProdutoDAO.java
@@ -0,0 +1,69 @@
+package banco;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import modelo.Produto;
+
+public class ProdutoDAO {
+	
+	private Connection conexao;
+	
+	public ProdutoDAO() {
+		
+		conexao = new FabricaDeConexao().pegaConexao();
+		
+	}
+	
+	public void registrar (Produto produto)throws SQLException {
+		
+		PreparedStatement stmt = null;
+		
+		try {
+			stmt = conexao.prepareStatement(" insert into produto values(null, ?, ?, ?, ?, ?");
+
+			stmt.setString(1, produto.getNmProduto());
+			stmt.setString(2, produto.getDescProduto());
+			stmt.setInt(3, produto.getQuantidade());
+			stmt.setDouble(4, produto.getPreco());
+			stmt.setString(5, produto.getImg());
+			
+			stmt.executeUpdate();
+			
+		} finally {
+			if (stmt != null)
+				stmt.close();
+			if (conexao != null)
+				conexao.close();
+		}
+	
+	}
+
+	
+    public void alterar(Produto produto) throws SQLException{
+    	
+    	 PreparedStatement stmt = null;
+    	 
+    	 try {
+    		 stmt = conexao.prepareStatement("update produto set nmProduto =?, descProduto = ?, quantidade = ?, preco = ?,  img= ? where id= ?");
+    		 
+    		stmt.setString(1, produto.getNmProduto());
+ 			stmt.setString(2, produto.getDescProduto());
+ 			stmt.setInt(3, produto.getQuantidade());
+ 			stmt.setDouble(4, produto.getPreco());
+ 			stmt.setString(5, produto.getImg());
+ 			
+ 			stmt.executeUpdate();
+			
+ 		} finally {
+ 			if (stmt != null)
+ 				stmt.close();
+ 			if (conexao != null)
+ 				conexao.close();
+ 		}
+    		 
+     }
+    
+   }
+
