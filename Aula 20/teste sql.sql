4ef041926a8f27db7cdf7928dfbe28d25752fd32
Adm Bergson <Adm Bergson@MariaAuxiliadora>
2018-07-25 12:29:00 -0300

    auala 20

---
+++ Aula 20/teste sql.sql
@@ -0,0 +1,17 @@
+create schema if not exists aula19;
+
+use aula19;
+
+create table if not exists cliente(
+
+	id int not null unique,
+	nome varchar(255) not null,
+	primary key(id)
+);
+
+create table if not exists telefone(
+	id int not null unique,
+	cid int not null,
+	numero varchar(15) not null,
+	foreign key(cid) references cliente(id)
+);
